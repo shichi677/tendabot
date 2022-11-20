22461ac0cf19b756f614d10a6b1136d5fa525198
shichi677 <116809203+shichi677@users.noreply.github.com>
2022-11-20 19:29:34 +0900

    :wastebacket: delete testfile

--- get_data_from_web_test.py
+++
@@ -1,8 +0,0 @@
-# from modules import get_spec_from_wiki_MS_page
-from modules import clanmatch_schedule
-
-# print(GFW.get_MS_page_url_from_wiki())
-# URL = "https://w.atwiki.jp/battle-operation2/pages/121.html"
-# print(get_spec_from_wiki_MS_page(URL))
-clanmatch_info = clanmatch_schedule()
-print(clanmatch_info)
