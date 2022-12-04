f51d34124c70ff9edd084d150a20266661a08134
shichi677 <slowlyshichi@gmail.com>
2022-12-04 21:38:50 +0900

    :sparkles: support openjtalk alphabet

--- modules/CreateText.py
+++ modules/CreateText.py
@@ -1,4 +1,53 @@
 import re
+import alkana
+import romkan
+import splitter
+
+alkana.add_external_data("./alkana_dict.csv")
+
+# reference https://mackro.blog.jp/archives/8479732.html
+def replace_alphabet_to_kana(text):
+
+    output = ""
+
+    # search word of 3 character or more and to kana
+    while word := re.search(r"[a-z-A-Z]{3,}", text):
+        output += text[: word.start()] + word_to_kana(word.group())
+        text = text[word.end() :]
+
+    return output + text
+
+
+def word_to_kana(word):
+
+    # if English word
+    if kana := alkana.get_kana(word.lower()):
+        return kana
+
+    else:
+        hiraganized_word = romkan.to_katakana(word.lower())
+
+        # if not ro-maji
+        if re.search("[a-zA-Z]+", hiraganized_word):
+            output = ""
+            split_result = splitter.split(word.lower())
+
+            # if compound word
+            if len(split_result) > 1:
+                # to kana by each splited word
+                for splited_word in split_result:
+                    output += word_to_kana(splited_word)
+
+                return output
+
+            # if not compund word
+            elif len(split_result) == 1:
+                return word
+
+        # if ro-maji
+        else:
+            return hiraganized_word
+
 
 # 絵文字IDは読み上げない
 def remove_custom_emoji(text):
@@ -9,12 +58,11 @@ def remove_custom_emoji(text):
 # URLなら省略
 def urlAbb(text):
     pattern = r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
-    exist_url = False
+    if re.search(pattern, text):
+        return "URLが送信されました " + re.sub(pattern, "", text)
 
-    if re.match(pattern, text):
-        exist_url = True
-
-    return exist_url, re.sub(pattern, "URLが送信されました", text)  # 置換処理
+    else:
+        return text
 
 
 def custom_process(text):
@@ -22,18 +70,11 @@ def custom_process(text):
     return re.sub(pattern, "", text)  # 置換処理
 
 
-def str_num_limit(text):
-    return text[:30] + "以下略"
-
-
 # message.contentをテキストファイルに書き込み
-def CreateText(inputText):
-    inputText = remove_custom_emoji(inputText)  # 絵文字IDは読み上げない
-    inputText = custom_process(inputText)
+def CreateText(input_text):
+    output_text = remove_custom_emoji(input_text)
+    output_text = custom_process(output_text)
+    output_text = urlAbb(output_text)
+    output_text = replace_alphabet_to_kana(output_text)
 
-    exist_url, outputText = urlAbb(inputText)  # URLなら省略
-
-    if len(outputText) > 30:
-        outputText = str_num_limit(outputText)
-
-    return outputText
+    return output_text[:30]
